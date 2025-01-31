698a4473bca4b5eb7b8024ca1d91b4b51d53cd62
lawrence <chylau12@gmail.com>
2025-01-31 21:05:57 +0100

    middleware options

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { SongsService } from './songs/songs.service';
 import { SongsModule } from './songs/songs.module';
 import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
+import { SongsController } from './songs/songs.controller';
 
 @Module({
   imports: [SongsModule],
@@ -12,6 +13,12 @@ import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('songs');
+    // consumer.apply(LoggerMiddleware).forRoutes('songs');  option 1
+    // consumer.apply(LoggerMiddleware).forRoutes({
+    //   path: 'songs',
+    //   method: RequestMethod.POST,
+    // }); //option 2, select method that should have the middlewares
+
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController); //Option 3 use for the entire controller
   }
 }
