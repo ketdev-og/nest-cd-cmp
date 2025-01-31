ba8714c436267b41574a14ae3db19062c1cce8e8
lawrence <chylau12@gmail.com>
2025-01-31 21:55:32 +0100

    service as a mcok service

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -2,13 +2,18 @@ import { Module } from '@nestjs/common';
 import { SongsController } from './songs.controller';
 import { SongsService } from './songs.service';
 
+const mockService = {
+  findAll() {
+    return [{ id: 1, title: 'ketem-mock' }];
+  },
+};
 @Module({
   controllers: [SongsController],
   providers: [
-    // SongsService
+    SongsService,
     {
       provide: SongsService,
-      useClass: SongsService,
+      useValue: mockService,
     },
   ],
 })
